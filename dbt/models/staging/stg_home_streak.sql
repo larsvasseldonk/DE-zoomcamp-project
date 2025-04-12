0bb442be06c33768a83d177fc154addfdcbf3c47
tansongkai <songkai93@hotmail.com>
2025-04-13 03:18:54 +0800

    update dbt

--- dbt/models/staging/stg_home_streak.sql
+++ dbt/models/staging/stg_home_streak.sql
@@ -3,7 +3,3 @@ SELECT
     team,
     {{ streak('result') }} as home_result 
 FROM {{ ref('stg_home') }}
-
-
-SELECT date,team,case when result<>"Lose" then "Unbeaten" else "Loss" end as away_result FROM `capstone-455515.capstone_dataset_2025.premierleague_awaydata`
-where side="Away";
