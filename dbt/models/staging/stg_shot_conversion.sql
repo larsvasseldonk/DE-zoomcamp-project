3777ec72d9886c33a7c598dcfee2b865268915e2
tansongkai <songkai93@hotmail.com>
2025-04-13 23:34:48 +0800

    modify dbt

--- dbt/models/staging/stg_shot_conversion.sql
+++ dbt/models/staging/stg_shot_conversion.sql
@@ -1,11 +1,12 @@
-select 
+with cte as (select 
     season, 
     HomeTeam as team, 
     "Home" as side, 
     sum(FullTimeHomeTeamGoals) as goals_scored, 
     sum(HomeTeamShotsOnTarget) as shots_on_target 
-from {{ source('zoomcamp', 'premierleague_dataset') }}
-group by season, HomeTeam 
+from 
+    {{ source('zoomcamp', 'premierleague_dataset') }}
+group by season, HomeTeam
 union all
 select 
     season, 
@@ -13,5 +14,7 @@ select
     "Away" as side, 
     sum(FullTimeAwayTeamGoals) as goals_scored, 
     sum(AwayTeamShotsOnTarget) as shots_on_target 
-from {{ source('zoomcamp', 'premierleague_dataset') }}
-group by season, AwayTeam
+from
+     {{ source('zoomcamp', 'premierleague_dataset') }}
+group by season, AwayTeam)
+select*from cte
