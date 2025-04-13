e0b3c1806b447d169efaea5dc4ebc9f2ae698aa5
tansongkai <songkai93@hotmail.com>
2025-04-13 15:39:42 +0800

    modify dbt

--- dbt/models/core/fct_goals_conceded.sql
+++ dbt/models/core/fct_goals_conceded.sql
@@ -2,7 +2,7 @@ select
     season,
     team,
     sum(goals_conceded) as total_goals_conceded,
-    rank() over (order by sum(goals_conceded)) as goals_ranking 
+    rank() over (order by sum(goals_conceded)) as goals_conceded_ranking 
     from {{ ref('fct_ranking') }}
 group by season, team
 order by sum(goals_conceded)
--- dbt/models/core/fct_goals_scored.sql
+++ dbt/models/core/fct_goals_scored.sql
@@ -2,7 +2,7 @@ select
     season,
     team,
     sum(goals_scored) as total_goals_scored,
-    rank() over (order by sum(goals_scored) desc) as goals_ranking 
+    rank() over (order by sum(goals_scored) desc) as goals_scored_ranking 
 from {{ ref('fct_ranking') }}
 group by season, team
 order by sum(goals_scored) desc
